92cd5775dcbf958c66980ec03c5534f4b3372842
Exas4000 <75817424+Exas4000@users.noreply.github.com>
2025-04-27 10:29:53 +0000

    commit V0.3.1

--- SweetkinBackOnTrack.Plugin/Plugin.cs
+++ SweetkinBackOnTrack.Plugin/Plugin.cs
@@ -21,6 +21,10 @@ using TrainworksReloaded.Core.Impl;
 using TrainworksReloaded.Core.Interfaces;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace SweetkinBackOnTrack.Plugin
 {
@@ -44,5 +48,135 @@ namespace SweetkinBackOnTrack.Plugin
 
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         }
+    }
+
+    public sealed class RoomStateCapacityModifierGrav : RoomStateModifierBase, IRoomStateCapacityModifier
+    {
+        // Token: 0x0600284F RID: 10319 RVA: 0x0009829B File Offset: 0x0009649B
+        public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+        {
+            base.Initialize(roomModifierData, coreGameManagers);
+            this.capacityDelta = roomModifierData.GetParamInt();
+        }
+
+        // Token: 0x06002850 RID: 10320 RVA: 0x000982B1 File Offset: 0x000964B1
+        public int GetModifiedCapacity()
+        {
+            return this.capacityDelta;
+        }
+
+        // Token: 0x040011F3 RID: 4595
+        private int capacityDelta;
+    }
+
+    public sealed class RoomStateAddEffectPostCombatLobby : RoomStateModifierBase, IRoomStatePostCombatModifier, IRoomStateModifier, ILocalizationParamInt, ILocalizationParameterContext
+    {
+        // Token: 0x0600283F RID: 10303 RVA: 0x00097FB8 File Offset: 0x000961B8
+        public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+        {
+            base.Initialize(roomModifierData, coreGameManagers);
+            foreach (CardEffectData cardEffectData in roomModifierData.GetParamCardEffectData())
+            {
+                CardEffectState cardEffectState = Activator.CreateInstance<CardEffectState>();
+                cardEffectState.Setup(cardEffectData, null, null);
+                _effects.Add(cardEffectState);
+            }
+        }
+
+        // Token: 0x170002DA RID: 730
+        // (get) Token: 0x06002840 RID: 10304 RVA: 0x00098028 File Offset: 0x00096228
+        public bool CanApplyInPreviewMode
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        // Token: 0x06002841 RID: 10305 RVA: 0x0009802B File Offset: 0x0009622B
+        public IEnumerator PostCombat(RoomState room, ICoreGameManagers coreGameManagers)
+        {
+            CombatManager combatManager = coreGameManagers.GetCombatManager();
+            yield return base.ShowTriggeredVFX(room, coreGameManagers);
+            yield return combatManager.ApplyEffects(_effects, room.GetRoomIndex(), null, false, null, null, null, true, null, null, false, null, null, 1, null, false, CardTriggerType.OnDiscard, null, false);
+            yield break;
+        }
+
+        // Token: 0x040011EB RID: 4587
+        private List<CardEffectState> _effects = new List<CardEffectState>();
+    }
+
+    public sealed class RoomStateAddEffectPostCombatKitchen : RoomStateModifierBase, IRoomStatePostCombatModifier, IRoomStateModifier, ILocalizationParamInt, ILocalizationParameterContext
+    {
+        // Token: 0x0600283F RID: 10303 RVA: 0x00097FB8 File Offset: 0x000961B8
+        public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+        {
+            base.Initialize(roomModifierData, coreGameManagers);
+            foreach (CardEffectData cardEffectData in roomModifierData.GetParamCardEffectData())
+            {
+                CardEffectState cardEffectState = Activator.CreateInstance<CardEffectState>();
+                cardEffectState.Setup(cardEffectData, null, null);
+                _effects.Add(cardEffectState);
+            }
+        }
+
+        // Token: 0x170002DA RID: 730
+        // (get) Token: 0x06002840 RID: 10304 RVA: 0x00098028 File Offset: 0x00096228
+        public bool CanApplyInPreviewMode
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        // Token: 0x06002841 RID: 10305 RVA: 0x0009802B File Offset: 0x0009622B
+        public IEnumerator PostCombat(RoomState room, ICoreGameManagers coreGameManagers)
+        {
+            CombatManager combatManager = coreGameManagers.GetCombatManager();
+            yield return base.ShowTriggeredVFX(room, coreGameManagers);
+            yield return combatManager.ApplyEffects(_effects, room.GetRoomIndex(), null, false, null, null, null, true, null, null, false, null, null, 1, null, false, CardTriggerType.OnDiscard, null, false);
+            yield break;
+        }
+
+        // Token: 0x040011EB RID: 4587
+        private List<CardEffectState> _effects = new List<CardEffectState>();
+    }
+
+    public sealed class RoomStateAddEffectPostCombatSpa : RoomStateModifierBase, IRoomStatePostCombatModifier, IRoomStateModifier, ILocalizationParamInt, ILocalizationParameterContext
+    {
+        // Token: 0x0600283F RID: 10303 RVA: 0x00097FB8 File Offset: 0x000961B8
+        public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+        {
+            base.Initialize(roomModifierData, coreGameManagers);
+            foreach (CardEffectData cardEffectData in roomModifierData.GetParamCardEffectData())
+            {
+                CardEffectState cardEffectState = Activator.CreateInstance<CardEffectState>();
+                cardEffectState.Setup(cardEffectData, null, null);
+                _effects.Add(cardEffectState);
+            }
+        }
+
+        // Token: 0x170002DA RID: 730
+        // (get) Token: 0x06002840 RID: 10304 RVA: 0x00098028 File Offset: 0x00096228
+        public bool CanApplyInPreviewMode
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        // Token: 0x06002841 RID: 10305 RVA: 0x0009802B File Offset: 0x0009622B
+        public IEnumerator PostCombat(RoomState room, ICoreGameManagers coreGameManagers)
+        {
+            CombatManager combatManager = coreGameManagers.GetCombatManager();
+            yield return base.ShowTriggeredVFX(room, coreGameManagers);
+            yield return combatManager.ApplyEffects(_effects, room.GetRoomIndex(), null, false, null, null, null, true, null, null, false, null, null, 1, null, false, CardTriggerType.OnDiscard, null, false);
+            yield break;
+        }
+
+        // Token: 0x040011EB RID: 4587
+        private List<CardEffectState> _effects = new List<CardEffectState>();
     }
 }
