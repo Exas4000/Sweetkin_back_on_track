b840f96bb4c676fded97c2a3b06a873332764027
Exas4000 <75817424+Exas4000@users.noreply.github.com>
2025-04-27 05:59:31 -0400

    Added C# files to try compile/append build in codeSpace
    Added C# files to try append build in codeSpace

---
+++ SweetkinBackOnTrack.Plugin/RoomStateAddEffectPostCombatKitchen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SweetkinBackOnTrack.Plugin.Class
+{
+    public sealed class RoomStateAddEffectPostCombatKitchen : RoomStateModifierBase, IRoomStatePostCombatModifier, IRoomStateModifier, ILocalizationParamInt, ILocalizationParameterContext
+    {
+        // Token: 0x0600283F RID: 10303 RVA: 0x00097FB8 File Offset: 0x000961B8
+        [NullableContext(1)]
+        public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+        {
+            base.Initialize(roomModifierData, coreGameManagers);
+            foreach (CardEffectData cardEffectData in roomModifierData.GetParamCardEffectData())
+            {
+                CardEffectState cardEffectState = Activator.CreateInstance<CardEffectState>();
+                cardEffectState.Setup(cardEffectData, null, null);
+                _effects.Add(cardEffectState);
+            }
+        }
+
+        // Token: 0x170002DA RID: 730
+        // (get) Token: 0x06002840 RID: 10304 RVA: 0x00098028 File Offset: 0x00096228
+        public bool CanApplyInPreviewMode
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        // Token: 0x06002841 RID: 10305 RVA: 0x0009802B File Offset: 0x0009622B
+        [NullableContext(1)]
+        public IEnumerator PostCombat(RoomState room, ICoreGameManagers coreGameManagers)
+        {
+            CombatManager combatManager = coreGameManagers.GetCombatManager();
+            yield return base.ShowTriggeredVFX(room, coreGameManagers);
+            yield return combatManager.ApplyEffects(_effects, room.GetRoomIndex(), null, false, null, null, null, true, null, null, false, null, null, 1, null, false, CardTriggerType.OnDiscard, null, false);
+            yield break;
+        }
+
+        // Token: 0x040011EB RID: 4587
+        [Nullable(1)]
+        private List<CardEffectState> _effects = new List<CardEffectState>();
+    }
+}
---
+++ SweetkinBackOnTrack.Plugin/RoomStateCapacityModifierGrav.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+// Token: 0x020004A8 RID: 1192
+public sealed class RoomStateCapacityModifierGrav : RoomStateModifierBase, IRoomStateCapacityModifier
+{
+	// Token: 0x0600284F RID: 10319 RVA: 0x0009829B File Offset: 0x0009649B
+	[NullableContext(1)]
+	public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+	{
+		base.Initialize(roomModifierData, coreGameManagers);
+		this.capacityDelta = roomModifierData.GetParamInt();
+	}
+
+	// Token: 0x06002850 RID: 10320 RVA: 0x000982B1 File Offset: 0x000964B1
+	public int GetModifiedCapacity()
+	{
+		return this.capacityDelta;
+	}
+
+	// Token: 0x040011F3 RID: 4595
+	private int capacityDelta;
+}
